d0390a338d5b06edc361a3a7197621b3474dace8
orin635 <omcdonogh635@gmail.com>
2024-04-30 12:30:14 +0100

    added gizmos

--- GE2-Test-Godot/animate_tentacle.gd
+++ GE2-Test-Godot/animate_tentacle.gd
@@ -11,23 +11,21 @@ var rand_dir = randf_range(-1, 1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	if(rand_dir >= 0):
-		rand_dir = 1
-	elif(rand_dir < 0):
-		rand_dir = -1
-	print(rand_dir)
-	var change_color = 0
-	var tentacle_pos = 1.1
-	for i in tentacle_length:
-		var part = CSGBox3D.new()
-		part.position = Vector3(tentacle_pos * i, 0,0)
-		part.size = Vector3(1,0.1,1)
-		newMaterial.albedo_color = Color.from_hsv(randf_range(0, 1), 1, 1, 0.8)
-		part.material = newMaterial
-		add_child(part)
-
-
+	if not Engine.is_editor_hint():
+		if(rand_dir >= 0):
+			rand_dir = 1
+		elif(rand_dir < 0):
+			rand_dir = -1
+		print(rand_dir)
+		var change_color = 0
+		var tentacle_pos = 1.1
+		for i in tentacle_length:
+			var part = CSGBox3D.new()
+			part.position = Vector3(tentacle_pos * i, 0,0)
+			part.size = Vector3(1,0.1,1)
+			newMaterial.albedo_color = Color.from_hsv(randf_range(0, 1), 1, 1, 0.8)
+			part.material = newMaterial
+			add_child(part)
 
 
 var change_color = 0
@@ -35,31 +33,37 @@ var last_pos_y = 0.00
 var increment_amount = 0.001 * rand_dir
 
 func _process(delta):
-	var all_parts = get_children()
-	change_color = change_color + 1
-	if(change_color == 15):
-		newMaterial.albedo_color = Color.from_hsv(randf_range(0, 1), 1, 1, 0.8)
+	if not Engine.is_editor_hint():
+		var all_parts = get_children()
+		change_color = change_color + 1
+		if(change_color == 15):
+			newMaterial.albedo_color = Color.from_hsv(randf_range(0, 1), 1, 1, 0.8)
+			for part in all_parts:
+				if part == CSGBox3D:
+					part.material = newMaterial
+			change_color = 0
+		
+		var part_num = 0
+		var last_part_pos = Vector3(0,0,0)
 		for part in all_parts:
-			if part == CSGBox3D:
-				part.material = newMaterial
-		change_color = 0
-	
-	var part_num = 0
-	var last_part_pos = Vector3(0,0,0)
-	for part in all_parts:
-		if part.is_class("CSGBox3D"):
-			var current_pos = part.position
-			var y_increase = increment_amount * part_num * part_num
-			current_pos.y = current_pos.y + y_increase
-			part.transform.origin = current_pos
-			part_num = part_num + 1
+			if part.is_class("CSGBox3D"):
+				var current_pos = part.position
+				var y_increase = increment_amount * part_num * part_num
+				current_pos.y = current_pos.y + y_increase
+				part.transform.origin = current_pos
+				part_num = part_num + 1
+				
+				part.rotation = Vector3(0,0,part.position.y/(tentacle_length/2.2))
+				
+				last_part_pos = current_pos
+				if(current_pos.y > max_extension or current_pos.y < -max_extension):
+					increment_amount = increment_amount * -1
 			
-			part.rotation = Vector3(0,0,part.position.y/(tentacle_length/2.2))
-			
-			last_part_pos = current_pos
-			if(current_pos.y > max_extension or current_pos.y < -max_extension):
-				increment_amount = increment_amount * -1
-			
+	else:
+		var tentacle_pos = 1.1
+		for i in tentacle_length:
+			DebugDraw3D.draw_box(Vector3(tentacle_pos*i,0,0),global_transform.basis.get_rotation_quaternion(),Vector3(1,0.1,1), Color.AQUA, true)
+
 	
 	
 	
--- GE2-Test-Godot/octopus_creator.gd
+++ GE2-Test-Godot/octopus_creator.gd
@@ -4,20 +4,23 @@ extends Node3D
 var radius = 1
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if not Engine.is_editor_hint():
-		var initial_rotate_amount = 360 / num_tentacles
-		var rotate_amount = initial_rotate_amount
+	var initial_rotate_amount = 360 / num_tentacles
+	var rotate_amount = initial_rotate_amount
+	var initial_pos = Vector3(0,0,0)
+	for i in num_tentacles:
+		var tentacle:Node3D
+		tentacle = tentacle_scene.instantiate()
+		tentacle.position = initial_pos
+		print(rotate_amount)
+		print(deg_to_rad(rotate_amount))
+		tentacle.rotation = Vector3(0,deg_to_rad(rotate_amount),0)
+		rotate_amount = rotate_amount + initial_rotate_amount
+
+		add_child(tentacle)
+
+		# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Engine.is_editor_hint():
 		var initial_pos = Vector3(0,0,0)
 		for i in num_tentacles:
-			var tentacle:Node3D
-			tentacle = tentacle_scene.instantiate()
-			tentacle.position = initial_pos
-			print(rotate_amount)
-			print(deg_to_rad(rotate_amount))
-			tentacle.rotation = Vector3(0,deg_to_rad(rotate_amount),0)
-			rotate_amount = rotate_amount + initial_rotate_amount
-
-			add_child(tentacle)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+			DebugDraw3D.draw_box(initial_pos,global_transform.basis.get_rotation_quaternion(),Vector3(1,0.1,1), Color.AQUA, true)
