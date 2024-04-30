747662a30f593755ba6bc0fce9955d1fff85ae0a
orin635 <omcdonogh635@gmail.com>
2024-04-30 09:52:28 +0100

    first commit

---
+++ GE2-Test-Godot/octopus_creator.gd
@@ -0,0 +1,12 @@
+extends Node3D
+@export var num_tentacles:int=8
+@export var tentacle_length:int=5
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
